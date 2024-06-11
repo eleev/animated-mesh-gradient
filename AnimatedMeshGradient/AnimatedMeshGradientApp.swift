4a405c5e3c7c976bf0576654eff744e9b88d1acc
Astemir <astemireleev@gmail.com>
2024-06-11 16:01:12 +0300

    Initial Commit

---
+++ AnimatedMeshGradient/AnimatedMeshGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedMeshGradientApp.swift
+//  AnimatedMeshGradient
+//
+//  Created by Astemir Eleev on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedMeshGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
